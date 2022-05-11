e6c1b12756e2f4454754e292085501d8077cc52b
SaraOshritYosian <saraoshr@g.jct.ac.il>
2022-05-11 19:15:45 +0300

    create 2 classes

--- CCTech/App.xaml.cs
+++ CCTech/App.xaml.cs
@@ -13,5 +13,6 @@ namespace CCTech
     /// </summary>
     public partial class App : Application
     {
+
     }
 }
--- CCTech/MainWindow.xaml.cs
+++ CCTech/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace CCTech
         public MainWindow()
         {
             InitializeComponent();
+            
         }
     }
 }
