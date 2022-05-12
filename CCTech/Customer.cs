d3c508e945d1bf1790d93f236b6d7f5bb53bf6e2
SaraOshritYosian <saraoshr@g.jct.ac.il>
2022-05-12 14:46:37 +0300

    ..

--- CCTech/Customer.cs
+++ CCTech/Customer.cs
@@ -11,6 +11,9 @@ namespace CCTech
 	{
 		const double butteryWeightPrecents = 0.7;
 		const double hoursWeightPrecents = 0.3;
+		 DateTime feeTimeStart;
+		DateTime feeTimeEnd;
+		const double percentsPerHour = 0.25;//אחוז טעינה לדקת הטענה
 		private DateTime exitTime;//To calculate his priority
 		private DateTime entryTime;//Obsolescence
 		private string name;//For the message
@@ -22,9 +25,9 @@ namespace CCTech
 		public static int nuberOfCustomers = 0;
 
 		public Customer()
-        {
+		{
 			nuberOfCustomers++;
-        }
+		}
 		public void SetDepartureTime(DateTime newDepartureTime)
 		{
 			exitTime = newDepartureTime;
@@ -85,19 +88,23 @@ namespace CCTech
 			return fineAmount;
 		}
 
-        public void SetCurrentPercentage(int newCurrentPercentage)
+		public void SetCurrentPercentage(int newCurrentPercentage)
 		{
 			currentPercentage = newCurrentPercentage;
 		}
 
-        public int GetCurrentPercentage()
+		public int GetCurrentPercentage()
 		{
 			return currentPercentage;
 		}
-        public int HefreshMinuts()
+		public int HefreshMinuts()
 		{
 			return exitTime.Hour * 60 + exitTime.Minute - entryTime.Hour * 60 + entryTime.Minute;
 
+		}
+		public int HefreshMinutsForFee()
+        {
+			return feeTimeEnd.Hour * 60 + feeTimeEnd.Minute - feeTimeStart.Hour * 60 + feeTimeStart.Minute;
 		}
 		public void RaundedHefreshHours()
 		{
@@ -125,10 +132,69 @@ namespace CCTech
 			return ((double)currentPercentage) / 100;
 		}
 		public void CalcPriority()
-        {
-			priority = butteryWeightPrecents * PrecentsNormalize() + hoursWeightPrecents* HoursNormalize();
+		{
+			priority = butteryWeightPrecents * PrecentsNormalize() + hoursWeightPrecents * HoursNormalize();
 
 		}
+
+		 public int sendMassage(Customer c)//שולחת הודעה ומחזירה את סכום הקנס
+        {
+
+			Console.WriteLine("Come and pick up your car,fees will be applied for arriving late");// או לפתוח חלון
+			Console.WriteLine("when you arrived please enter 1");
+			int answere = int.Parse(Console.ReadLine());
+			while (answere!=1)
+            {
+
+            }
+			//הגיע
+
+			feeTimeEnd = DateTime.Now;
+			int sumTimeByMinuts = HefreshMinutsForFee();
+			if(sumTimeByMinuts<15)
+            {
+				return 0;
+            }
+			if(sumTimeByMinuts>=15&& sumTimeByMinuts<30)
+            {
+				return 15; 
+            }
+			if (sumTimeByMinuts >= 30 && sumTimeByMinuts < 60)
+			{
+				return 50;
+			}
+			if (sumTimeByMinuts >= 60)
+			{
+				return 100;
+			}
+			return 0;
+			//Console.WriteLine("Did you arrive?");
+			//string answer = Console.ReadLine();
+
+		}
+		public void charging(int wantedHours)//מטעינה לפי מספר השעות הרצויות
+        {
+			int wantedPrecent = wantedHours * 15;//כמה אחוזים השעות טעינה האלו שוות
+			while(currentPercentage< wantedPrecent-3)//כי אני שולחת הודעה בשלושה האחרונים שיבוא לקחת
+            {
+				currentPercentage = (int) (HefreshMinuts() * percentsPerHour);
+
+			}
+			//הטענו פחות 3 אחוזים
+			fineAmount=sendMassage(this);
+			feeTimeStart = DateTime.Now;
+			currentPercentage += 3;//מוסיף את האחוזים הנותרים להטענה רצויה
+			if(fineAmount>=15&& fineAmount<100)
+            {
+				Console.WriteLine("You have a fee in amount of" + fineAmount);
+            }
+            else
+            {
+				Console.WriteLine("Your car will be taken");
+			}
+			Console.WriteLine("Your car is charged succesfully!");
+		}
+
 	}
 	
 }
